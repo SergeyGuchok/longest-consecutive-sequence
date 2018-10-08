46b05de533429368feaaf465753c4d433a565e41
Sergey <gck.sergey@gmail.com>
2018-10-08 17:19:20 +0300

    made with Set

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
-	array = array.sort((a,b)=>a-b);
-	let count = 1;
-	let score = 1;
-	for(let i = 0; i<array.length-1; i++){
-		if(array[i]==array[i+1]-1){
-			count++;
-		}else{ count = 1;}
-		if(count>score){
-			score = count;
-		}
-		
-	}
-	return array.length == 0 ? 0 : score;
+	if(array.length==0){return 0}
+ 	
+ 	let lengths=1;	
+	let count=1;
+
+	let set = new Set(array);
+
+ 	set.forEach(function(item) {
+ 		let plus =1+ item;
+ 		let minus = 1- item;
+ 
+ 		while(set.has(plus)){
+ 			plus++;
+ 			count++
+ 			if(count>lengths)lengths=count;
+ 		}
+ 		while(set.has(minus)){
+ 			minus--;
+ 			count++;
+ 			if(count>lengths) lengths=count;
+ 		}
+ 		count = 1;
+ 	}
+ )
+ return lengths;
 }
